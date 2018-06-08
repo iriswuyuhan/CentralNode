f1a08dfde0bf8c2d6d104f152685d56e62f1feff
wxf <745040090@qq.com>
2018-06-09 00:47:03 +0800

    实现访问 SystemC 的接口

--- center/src/main/java/com/integrated/center/dao/SystemC.java
+++ center/src/main/java/com/integrated/center/dao/SystemC.java
@@ -1,4 +1,54 @@
 package com.integrated.center.dao;
 
+import com.integrated.center.model.Choices;
+import com.integrated.center.model.Courses;
+import com.integrated.center.model.Students;
+import com.integrated.center.util.CallInterface;
+
+import static com.integrated.center.util.DataConstant.C_URL;
+
 public class SystemC {
+    public Courses getAllCourses(){
+        String courses = CallInterface.interfaceUtil(C_URL + "/getAllCourses", "");
+        return null;
+    }
+
+    public Students getAllStudents(){
+        String students = CallInterface.interfaceUtil(C_URL + "/getAllStudents", "");
+        return null;
+    }
+
+    public Choices getAllChoices(){
+        String choices = CallInterface.interfaceUtil(C_URL + "/getAllElectives", "");
+        return null;
+    }
+
+    public Boolean chooseCourse(String sId, String cId){
+        String res = CallInterface.interfaceUtil(C_URL + "/addCourse", "");
+        return null;
+    }
+
+    public Boolean quitCourse(String sId, String cId){
+        String res = CallInterface.interfaceUtil(C_URL + "/quitCourse", "");
+        return null;
+    }
+
+    /**
+     *
+     * @param student 待增加学生信息的 xml
+     * @return
+     */
+    public Boolean addStudent(String student){
+        String res = CallInterface.interfaceUtil(C_URL + "/addStudent", student);
+        return null;
+    }
+
+    /**
+     * test
+     * @param args
+     */
+    public static void main(String[] args){
+        SystemC systemC = new SystemC();
+        systemC.getAllCourses();
+    }
 }
